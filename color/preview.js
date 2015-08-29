cc6ed6454102adbb9b92a47a9ca11b8412025586
docwilmot <docwilmot@gmail.com>
2015-08-29 16:38:09 -0500

    Style and color fixes

--- color/preview.js
+++ color/preview.js
@@ -13,25 +13,28 @@
         $('div').remove('#preview-logo');
       }
 
-      // Solid background.
-      $('#preview', form).css('backgroundColor', $('#palette input[name="palette[bg]"]', form).val());
-
       // Text preview.
       $('#preview #preview-main h2, #preview .preview-content', form).css('color', $('#palette input[name="palette[text]"]', form).val());
       $('#preview #preview-content a', form).css('color', $('#palette input[name="palette[link]"]', form).val());
 
-      // Sidebar block.
-      $('#preview #preview-sidebar #preview-block', form).css('background-color', $('#palette input[name="palette[sidebar]"]', form).val());
-      $('#preview #preview-sidebar #preview-block', form).css('border-color', $('#palette input[name="palette[sidebarborders]"]', form).val());
+      // Sidebar.
+      $('#preview #preview-sidebar', form).css('background-color', $('#palette input[name="palette[sidebar]"]', form).val());
+      
+      // Main
+      $('#preview #preview-content', form).css('background-color', $('#palette input[name="palette[main]"]', form).val());
+
+      // Article
+      $('#preview #preview-node', form).css('background-color', $('#palette input[name="palette[article]"]', form).val());
+
+      // Article border
+      $('#preview #preview-node, #preview #preview-main, #preview #preview-content', form).css('border-color', $('#palette input[name="palette[articleborder]"]', form).val());
+
+      // Header background.
+      $('#preview #preview-header', form).css('background-color', $('#palette input[name="palette[header]"]', form).val());
 
       // Footer wrapper background.
       $('#preview #preview-footer-wrapper', form).css('background-color', $('#palette input[name="palette[footer]"]', form).val());
 
-      // CSS3 Gradients.
-      var gradient_start = $('#palette input[name="palette[top]"]', form).val();
-      var gradient_end = $('#palette input[name="palette[bottom]"]', form).val();
-
-      $('#preview #preview-header', form).attr('style', "background-color: " + gradient_start + "; background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(" + gradient_start + "), to(" + gradient_end + ")); background-image: -moz-linear-gradient(-90deg, " + gradient_start + ", " + gradient_end + ");");
 
       $('#preview #preview-site-name', form).css('color', $('#palette input[name="palette[titleslogan]"]', form).val());
     }
